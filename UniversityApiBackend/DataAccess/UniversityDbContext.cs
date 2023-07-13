d78a18e6055e9ad418d1be48749e3407fec07e83
lucasribeiro1024b@gmail.com <lucasribeiro1024b@gmail.com>
2023-07-13 15:25:32 -0300

    Add project files.

---
+++ UniversityApiBackend/DataAccess/UniversityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets (Database Tables)
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Program.cs
@@ -0,0 +1,37 @@
+// EntitiyFramework
+using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// TODO: DB connection
+const string CONNECTIONNAME = "UniversityDb";
+var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+// Context
+builder.Services.AddDbContext<UniversityDbContext>(options => options.UseSqlServer(connectionString));
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
